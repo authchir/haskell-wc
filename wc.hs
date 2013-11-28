a8fde1c0455dae3d6b82ef6a093141e1137824bd
Martin Desharnais <martin.desharnais@gmail.com>
2013-11-27 23:17:16 -0500

    Add support for program options --help and --version

--- wc.hs
+++ wc.hs
@@ -1,8 +1,12 @@
+module Main (main) where
+
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.ByteString.Lazy.Char8 as BSChar8
 import Data.Int
 import Data.List
+import System.Console.GetOpt
 import System.Environment
+import System.IO
 import Text.Printf
 
 type LineCount = Int64
@@ -10,15 +14,35 @@ type WordCount = Int64
 type CharCount = Int64
 type Padding = Int
 
+data Flag = Help | Version
+          deriving (Show)
+
+options :: [OptDescr Flag]
+options =
+    [ Option [] ["help"] (NoArg Help) "display this help and exit"
+    , Option [] ["version"] (NoArg Version) "output version information and exit"
+    ]
+
 main :: IO ()
 main = do
     args <- getArgs
-    case args of
-        [] -> fmap (("usage: "++) . (++" file...")) getProgName >>= putStrLn
+    case getOpt Permute options args of
+        (opts,nonOpts,[]) -> runWithOptionsAndArgs opts nonOpts --mapM_ print opts >> putStrLn "" >> mapM_ print nonOpts
+        (_,_,errs) -> hPutStr stdout $ "Error:\n" ++ intercalate "\n" errs
+
+runWithOptionsAndArgs :: [Flag] -> [String] -> IO ()
+runWithOptionsAndArgs (Help:_) _ = printHelp
+runWithOptionsAndArgs (Version:_) _ = putStrLn "haskell-wc 0.5"
+runWithOptionsAndArgs [] [] = printHelp
+runWithOptionsAndArgs [] args = 
+    case args of 
         [file] -> fmap (wcFile file) (BS.readFile file) >>= putStrLn
         files -> fmap (wcFiles files) (mapM BS.readFile files) >>= putStrLn
     where wcFile name = intercalate "\n" . init . processContents [name] . (:[])
           wcFiles names = intercalate "\n" . processContents names
+
+printHelp :: IO ()
+printHelp = fmap ((flip usageInfo) options . ("usage: "++) . (++" [OPTION]... file...")) getProgName >>= putStr
 
 processContents :: [FilePath] -> [BS.ByteString] -> [String]
 processContents files contents = formatWcAll padding files stats
